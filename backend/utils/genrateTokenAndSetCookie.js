71adb099311f2504219c52cb99c42c99e65b3a2e
ADITYA SHARMA <aditya3124sharma@gmail.com>
2025-07-12 21:50:42 +0530

    Update genrateTokenAndSetCookie.js

--- backend/utils/genrateTokenAndSetCookie.js
+++ backend/utils/genrateTokenAndSetCookie.js
@@ -2,13 +2,13 @@ import jwt from 'jsonwebtoken';
 
 export const genrateTokenAndSetCookie = (res, newUserId) => {
   const token = jwt.sign({ id: newUserId }, process.env.JWT_SECRET, {
-    expiresIn: '7d', // Token expires in 1 day
+    expiresIn: '7d', 
   });
 
   res.cookie('token', token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production', // Use secure cookies in production
-    maxAge: 7*24 * 60 * 60 * 1000, // Cookie expires in 1 day
+    maxAge: 7*24 * 60 * 60 * 1000, 
     sameSite: 'Strict', // Helps prevent CSRF attacks
 
   });
